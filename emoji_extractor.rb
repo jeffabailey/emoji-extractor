77a48af4ed1b4734f94eb503b2435d1a0f4cdee2
Jeff Bailey <776901+jeffabailey@users.noreply.github.com>
2023-10-07 15:16:31 -0700

    If a git tree falls in a forest and no one is around to hear it, does it make a sound?

--- emoji_extractor.rb
+++ emoji_extractor.rb
@@ -3,47 +3,16 @@
 
 require 'stringio'
 require 'fileutils'
-
-def extract_chunk(input, output)
-  lenword = input.read(4)
-  length = lenword.unpack('N')[0]
-  type = input.read(4)
-  data = length>0 ? input.read(length) : ""
-  crc = input.read(4)
-  return nil if length<0 || !(('A'..'z')===type[0,1])
-  #return nil if validate_crc(type+data, crc)
-  output.write lenword
-  output.write type
-  output.write data
-  output.write crc
-  return [type, data]
-end
+require 'chunky_png'
 
 def extract_png(input)
-  buf = StringIO.new
-  hdr = input.read(8)
-  raise "Not a PNG File" if hdr[0,4]!= "\211PNG"
-  raise "file not in binary mode" if hdr[4,4]!="\r\n\032\n"
-  buf.write(hdr)
+  chunk_data = ChunkyPNG::Image.from_io(input)
+  height, width = chunk_data.height, chunk_data.width
 
-  height, width = 0, 0
+  FileUtils.mkdir_p(dir = "images")
 
-  loop do
-    chunk_type, chunk_data = extract_chunk(input,buf)
-    height, width = chunk_data.unpack('NN') if chunk_type == 'IHDR'
-    break if  chunk_type.nil? || chunk_type == 'IEND'
-  end
+  chunk_data.save("#{dir}/#{Time.now.to_i}.png", :fast_rgba)
 
-  FileUtils.mkdir_p(dir = "images/#{height}x#{width}")
-  if @prev != dir
-    @n = 0
-    @prev = dir
-  end
-
-  buf.rewind
-  ofp = File.new("#{dir}/#{@n+=1}.png","wb")
-  ofp.write buf.read
-  ofp.close
 end
 
 begin
@@ -58,6 +27,7 @@ ttf_data = @ttf.read
 
 pos = 0
 while m = /\211PNG/.match(ttf_data[pos..-1])
+
   raise "no PNG found" if !m
   pos += m.begin(0) + 1
   @ttf.seek(pos-1)
